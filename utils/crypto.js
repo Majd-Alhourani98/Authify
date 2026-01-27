8898c17498b9611d0faba4bfb06b3f86c4784839
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2026-01-27 12:00:00 +0200

    feat: Add getExpiryDate helper for flexible TTL-based expiration

--- utils/crypto.js
+++ utils/crypto.js
@@ -4,10 +4,14 @@ const hashValue = value => {
   return crypto.createHash('sha256').update(value).digest('hex');
 };
 
+const getExpiryDate = ttlMs => {
+  return new Date(Date.now() + ttlMs);
+};
+
 const generateSecureOTP = () => {
   const otp = crypto.randomInt(0, 1_000_000).toString().padStart(6, '0');
   const hashedOTP = hashValue(otp);
-  const otpExpires = new Date(Date.now() + 15 * 60 * 1000);
+  const otpExpires = getExpiryDate(15 * 60 * 1000);
   return { otp, hashedOTP, otpExpires };
 };
 
