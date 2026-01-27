fc3d9f13d0d944f147daa4554be896c03146296b
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2026-01-27 12:00:00 +0200

    feat: Create hashValue helper using SHA-256 for consistent hashin

--- utils/crypto.js
+++ utils/crypto.js
@@ -1,8 +1,14 @@
 const crypto = require('crypto');
 
+const hashValue = value => {
+  return crypto.createHash('sha256').update(value).digest('hex');
+};
+
 const generateSecureOTP = () => {
   const otp = crypto.randomInt(0, 1_000_000).toString().padStart(6, '0');
-  const hashedOTP = crypto.createHash('sha256').update(otp).digest('hex');
+  const hashedOTP = hashValue(otp);
   const otpExpires = new Date(Date.now() + 15 * 60 * 1000);
   return { otp, hashedOTP, otpExpires };
 };
+
+console.log(generateSecureOTP());
